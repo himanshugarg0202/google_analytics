0dbb845b6dddb2773f45b8878b7badf95d3f8f2b
Himanshu Garg <himanshugarg@himanshu.garg>
2022-07-21 22:58:02 +0530

    forth changes

--- src/Common.jsx
+++ src/Common.jsx
@@ -9,7 +9,7 @@ const Common = (Props) =>{
                 <div className="col-10 mx-auto">
                 <div className='row'>
                    <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column" >
-                    <h1>{Props.name } <strong className="brand-name">DTDL Pvt. Ltd.</strong></h1>
+                    <h1>{Props.name } <strong className="brand-name">DT Digital Labs Pvt. Ltd.</strong></h1>
                     <h2 className="my-3"> We are the team of talented developers.</h2>
                         <div className="mt-3">
                             <NavLink to={Props.visit} className="btn-get-started">{Props.btname}</NavLink>
